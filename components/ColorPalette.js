a7273cb366cc0e9381dcedbdd4a00535b1cfd21d
Stephanie Tack <stephierathjen@gmx.de>
2024-09-09 20:13:49 +0200

    deleted second variant for luminance for color

--- components/ColorPalette.js
+++ components/ColorPalette.js
@@ -42,26 +42,3 @@ const StyledListItem = styled.li`
   border-radius: 50px;
   width: fit-content;
 `;
-
-// Zweite automatisierte Lösung. Hier wird die Helligkeit der Backgroundcolor mittels einer Funktion berechnet:
-// const StyledListItem = styled.li`
-//   background-color: ${(props) => props.color};
-//   color: ${(props) => (isColorDark(props.color) ? "#ffffff" : "#000000")};
-//   padding: 10px;
-//   border-radius: 50px;
-//   width: fit-content;
-// `;
-
-// function isColorDark(color) {
-//   const hex = color.replace("#", "");
-
-//   const r = parseInt(hex.substring(0, 2), 16);
-//   const g = parseInt(hex.substring(2, 4), 16);
-//   const b = parseInt(hex.substring(4, 6), 16);
-
-//   // Formel zur Berechnung der Luminanz basierend auf dem menschlichen Auge
-//   const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
-
-//   // Wenn die Luminanz gering ist, gilt die Farbe als dunkel (sprich weiße Schrift)
-//   return luminance < 186; // Schwellenwert
-// }
